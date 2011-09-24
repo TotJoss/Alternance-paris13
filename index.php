6442cb7f0520de32e1f0c9d9f8e7ec3d0cdf86d2
christophe lyraud <christophelyraud@gmail.com>
2011-09-24 17:35:53 +0200

    modification pour validation w3c

--- index.php
+++ index.php
@@ -2,49 +2,49 @@
 include('header.html');
 require 'connecte.php';
 $reponse = $bdd->query('SELECT * FROM `eleve`');
+$bdd->query("SET NAMES 'utf8'");
 $repTab = $reponse->fetchAll();
 
 
-echo"<p>pour afficher/supprimer uhne colonne, clqiuez sur son nom dans la liste</p>";
 
-echo '<table class="table1"><tr>
-<th class="id">id</th>
-<th class="civilité">civilité</th>
-<th class="prénom">prénom</th>
+echo"<p>pour afficher/supprimer une colonne, cliquez sur son nom dans la liste</p><br/>";
+include('sidebar.html');
+
+echo '<table class="table1"><tr><th class="id">id</th>
+<th class="civilitÃ©">civilitÃ©</th>
+<th class="prÃ©nom">prÃ©nom</th>
 <th class="nom">nom</th>
 <th class="date">date de naissance</th>
 <th class="entreprise">entreprise</th>
 <th class="divers">divers</th>
-<th class="nationalité">nationalité</th>
+<th class="nationalitÃ©">nationalitÃ©</th>
 <th class="rue">rue</th>
 <th class="ville">ville</th>
 <th class="code">code postal</th>
-<th class="téléphone">téléphone</th>
+<th class="tÃ©lÃ©phone">telephone</th>
 <th class="mail">mail</th>
-<th class="statut">statut</th></tr> </table>';
-
-include('sidebar.html');
+<th class="statut">statut</th></tr>';
 
 foreach ($repTab as $valTab)
 {	
 	echo '<tr>
 	<td class="id">'.$valTab['id'] .'</td>
-	<td class="civilité">'.$valTab['civilité'] .'</td>
-	<td class="prénom">'.$valTab['prénom'] .'</td>
+	<td class="civilitÃ©">'.$valTab['civilite'] .'</td>
+	<td class="prÃ©nom">'.$valTab['prenom'] .'</td>
 	<td class="nom">'.$valTab['nom'] .'</td>
 	<td class="date">'.$valTab['date de naissance'] .'</td>
 	<td class="entreprise">'.$valTab['entreprise'] .'</td>
 	<td class="divers">'.$valTab['divers'] .'</td>
-	<td class="nationalité">'.$valTab['nationalité'] .'</td>
+	<td class="nationalitÃ©">'.$valTab['nationalite'].'</td>
 	<td class="rue">'.$valTab['rue'] .'</td>
 	<td class="ville">'.$valTab['ville'] .'</td>
 	<td class="code">'.$valTab['code postal'] .'</td>
-	<td class="téléphone">'.$valTab['téléphone'] .'</td>
+	<td class="tÃ©lÃ©phone">'.$valTab['telephone'] .'</td>
 	<td class="mail">'.$valTab['mail'] .'</td>
 	<td  class="statut">'.$valTab['statut'] .'</td>
 	</tr>';
 }
-
+echo "</table>";
 include('footer.html');
 
 ?>
