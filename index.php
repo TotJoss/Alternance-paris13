c884b520844e0198011fbc28a77edee38afb0fd6
christophe lyraud <christophelyraud@gmail.com>
2011-09-24 15:59:51 +0200

    affichage infos élèves

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+include('header.html');
+require 'connecte.php';
+$reponse = $bdd->query('SELECT * FROM `eleve`');
+$repTab = $reponse->fetchAll();
+
+echo "<h1>élèves</h3>\n";
+
+echo"<p>pour afficher/supprimer uhne colonne, clqiuez sur son nom dans la liste</p>";
+
+echo '<table class="table1"><tr>
+<th class="id">id</th>
+<th class="civilité">civilité</th>
+<th class="prénom">prénom</th>
+<th class="nom">nom</th>
+<th class="date">date de naissance</th>
+<th class="entreprise">entreprise</th>
+<th class="divers">divers</th>
+<th class="nationalité">nationalité</th>
+<th class="rue">rue</th>
+<th class="ville">ville</th>
+<th class="code">code postal</th>
+<th class="téléphone">téléphone</th>
+<th class="mail">mail</th>
+<th class="statut">statut</th></tr> </table>';
+
+include('sidebar.html');
+
+foreach ($repTab as $valTab)
+{	
+	echo '<tr>
+	<td class="id">'.$valTab['id'] .'</td>
+	<td class="civilité">'.$valTab['civilité'] .'</td>
+	<td class="prénom">'.$valTab['prénom'] .'</td>
+	<td class="nom">'.$valTab['nom'] .'</td>
+	<td class="date">'.$valTab['date de naissance'] .'</td>
+	<td class="entreprise">'.$valTab['entreprise'] .'</td>
+	<td class="divers">'.$valTab['divers'] .'</td>
+	<td class="nationalité">'.$valTab['nationalité'] .'</td>
+	<td class="rue">'.$valTab['rue'] .'</td>
+	<td class="ville">'.$valTab['ville'] .'</td>
+	<td class="code">'.$valTab['code postal'] .'</td>
+	<td class="téléphone">'.$valTab['téléphone'] .'</td>
+	<td class="mail">'.$valTab['mail'] .'</td>
+	<td  class="statut">'.$valTab['statut'] .'</td>
+	</tr>';
+}
+
+include('footer.html');
+
+?>
